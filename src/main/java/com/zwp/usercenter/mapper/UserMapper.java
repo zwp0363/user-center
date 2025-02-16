dc1060897bcbff8f3f8506977b938e50e534e844
zwp <hello@zwp.cn>
2025-02-16 12:48:12 +0800

    后端初始化

---
+++ src/main/java/com/zwp/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.zwp.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zwp.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/test/java/com/zwp/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwp.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // 默认入口类与启动类名称一致，所以不用指定启动类或加@RunWith(SpringRunner.class)
+class UserCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
