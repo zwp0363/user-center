a35022d0366f6f72f2ee6ce8aabc6f17f1f5999f
zwp <hello@zwp.cn>
2025-03-06 18:36:08 +0800

    后端Knife4j接口文档以及网页内容抓取

---
+++ src/main/java/com/zwp/usercenter/once/importuser/ImportXingQiuUser.java
@@ -0,0 +1,36 @@
+package com.zwp.usercenter.once.importuser;
+
+import com.alibaba.excel.EasyExcel;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 导入星球用户到数据库
+ *
+ * @author zwp
+ */
+public class ImportXingQiuUser {
+
+    public static void main(String[] args) {
+        // todo 记得改为自己的测试文件
+        String fileName = "D:\\BaiduNetdiskDownload\\用户中心管理源码\\user-center\\src\\main\\resources\\testExcel.xlsx";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 同步读取会自动finish
+        List<XingQiuTableUserInfo> userInfoList =
+                EasyExcel.read(fileName).head(XingQiuTableUserInfo.class).sheet().doReadSync();
+        System.out.println("总数 = " + userInfoList.size());
+        Map<String, List<XingQiuTableUserInfo>> listMap =
+                userInfoList.stream()
+                        .filter(userInfo -> StringUtils.isNotEmpty(userInfo.getUsername()))
+                        .collect(Collectors.groupingBy(XingQiuTableUserInfo::getUsername));
+        for (Map.Entry<String, List<XingQiuTableUserInfo>> stringListEntry : listMap.entrySet()) {
+            if (stringListEntry.getValue().size() > 1) {
+                System.out.println("username = " + stringListEntry.getKey());
+                System.out.println("1");
+            }
+        }
+        System.out.println("不重复昵称数 = " + listMap.keySet().size());
+    }
+}
